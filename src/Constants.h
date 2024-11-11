6930b522ca3fca0ced04d5f576d15d26e7c2b1bd
jhesso <jhesso.dev@gmail.com>
2024-11-11 15:31:26 +0200

    added two new values: powerUp height and width. changed name of PlayerSpeed to PlayerDefaultSpeed

--- src/Constants.h
+++ src/Constants.h
@@ -1,11 +1,11 @@
 #pragma once
 
-const int ScreenWidth = 1600;
-const int ScreenHeight = 1200;
+const int ScreenWidth = 800;
+const int ScreenHeight = 600;
 
 const float PlayerHeight = 55.0f;
 const float PlayerWidth = 55.0f;
-const float PlayerSpeed = 155.0f;
+const float PlayerDefaultSpeed = 155.0f;
 
 const float VampireHeight = 32.0f;
 const float VampireWidth = 32.0f;
@@ -14,3 +14,6 @@ const float VampireSpeed = 100.0f;
 const float WeaponHeight = 10.0f;
 const float WeaponWidth = 100.0f;
 const float WeaponActiveTime = 0.25f;
+
+const float PowerUpHeight = 10.0f;
+const float PowerUpWidth = 10.0f;
