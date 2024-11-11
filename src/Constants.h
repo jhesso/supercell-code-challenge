ce0f92f9fdf32cdc3843ff533a9a0c9d3372a71b
jhesso <jhesso.dev@gmail.com>
2024-11-11 20:53:41 +0200

    reverted screen size back to original

--- src/Constants.h
+++ src/Constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-const int ScreenWidth = 800;
-const int ScreenHeight = 600;
+const int ScreenWidth = 1600;
+const int ScreenHeight = 1200;
 
 const float PlayerHeight = 55.0f;
 const float PlayerWidth = 55.0f;
