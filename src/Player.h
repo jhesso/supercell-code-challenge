7ea2b283752a67f5fe34706efcfb7681b02062e3
jhesso <jhesso.dev@gmail.com>
2024-11-11 15:36:41 +0200

    added a new variable: m_playerSpeed and a getter and setter for it. also added an include for Weapon.h cause I got a funny compilation error without it

--- src/Player.h
+++ src/Player.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Rectangle.h"
+#include "Weapon.h"
+#include "Constants.h"
 
 #include <memory>
 
@@ -20,7 +22,7 @@ class Player : public Rectangle
 public:
     Player(Game* pGame);
     virtual ~Player() {}
-    
+
     bool initialise();
     void move(InputData inputData, float deltaTime);
     void attack();
@@ -32,9 +34,13 @@ public:
 
     Weapon* getWeapon() { return m_pWeapon.get(); }
 
+    float getSpeed(void) { return m_playerSpeed; }
+    void setSpeed(float f) { m_playerSpeed = f; }
+
 private:
     bool    m_isDead = false;
     eDirection m_direction = LEFT;
     Game*   m_pGame;
+    float   m_playerSpeed = PlayerDefaultSpeed;
     std::unique_ptr<Weapon> m_pWeapon;
 };
